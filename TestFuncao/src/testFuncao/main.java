792a7f39b7c7b9296c5b3d12130c186c83ce8109
igor <igorkfz31@gmail.com>
2025-03-17 21:48:28 -0300

    AtribuindoFuncao

--- TestFuncao/src/testFuncao/main.java
+++ TestFuncao/src/testFuncao/main.java
@@ -7,25 +7,35 @@ public class main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("imprima o maior numero");
+		System.out.println("Enter three numbers");
 		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
 		
-		if (a > b && a > c) {
-			System.out.println("higher= " + a);
-		}
-		else if (b > c) {
-			System.out.println("higher= " + b);
-		}
-		else {
-			System.out.println("higher= " + c);
-		}
-		
+		int higher = max(a, b, c);
+		ShowResult(higher);
 		
 		sc.close();
 
+	}
+	// nao precisa ser o mesmo nome da variavel de cima 
+	public static int max(int x, int y, int z) {
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+		} else if (y > z) {
+			aux = y;
+		} else {
+			aux = z;
+		}
+		
+		return aux;	
+			
+	}
+	
+	public static void ShowResult(int value) {
+		System.out.println("higher= " + value);
 	}
 
 }
